7a8e31731fc65b1255948cdf3c9a2a899956e067
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2014-05-08 15:41:34 -0700

    Project Create
    Initial Empty Project

---
+++ TipCalculator/TipCalculator/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  TipCalculator
+//
+//  Created by Theofanis Pantelides on 5/8/14.
+//  Copyright (c) 2014 CodePath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Theofanis Pantelides on 5/8/14.
+//  Copyright (c) 2014 CodePath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}

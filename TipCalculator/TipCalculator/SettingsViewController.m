ef95fb4e8235c0df54ebd1be43e6086b800acfb5
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2014-05-11 23:58:19 -0700

    Comments
    Comments
    Minor refactoring

--- TipCalculator/TipCalculator/SettingsViewController.m
+++ TipCalculator/TipCalculator/SettingsViewController.m
@@ -39,48 +39,58 @@
 
 - (void) viewWillAppear:(BOOL)animated
 {
+    // Load User Defaults
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *btnTipIndex = [defaults objectForKey:@"btnTipIndex"];
     NSString *btnPeopleValue = [defaults objectForKey:@"btnPeopleValue"];
     NSString *btnRoundingIndex = [defaults objectForKey:@"btnRoundingIndex"];
     
+    // Check if value isSet
     if(btnTipIndex != nil)
     {
         self.btnTip.selectedSegmentIndex = [btnTipIndex floatValue];
     }
     
+    // Check if value isSet
     if(btnPeopleValue != nil)
     {
         self.btnPeople.value = [btnPeopleValue floatValue];
     }
     
+    // Check if value isSet
     if(btnRoundingIndex != nil)
     {
         self.btnRounding.selectedSegmentIndex = [btnRoundingIndex floatValue];
     }
     
+    // Update UI
     [self updateUI];
 }
 
 - (IBAction) onValueChange:(id)sender
 {
+    // Load UI values into variables
     int btnTipIndex = (int)self.btnTip.selectedSegmentIndex;
     int btnPeopleValue = self.btnPeople.value;
     int btnRoundingIndex = (int)self.btnRounding.selectedSegmentIndex;
     
+    // Save User Defaults
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:[NSString stringWithFormat:@"%d", btnTipIndex] forKey:@"btnTipIndex"];
     [defaults setObject:[NSString stringWithFormat:@"%d", btnPeopleValue] forKey:@"btnPeopleValue"];
     [defaults setObject:[NSString stringWithFormat:@"%d", btnRoundingIndex] forKey:@"btnRoundingIndex"];
     [defaults synchronize];
     
+    // Update UI
     [self updateUI];
 }
 
 - (void) updateUI
 {
+    // Load UI values into variables
     int totalPeople = self.btnPeople.value;
     
+    // Update UI with calculated values
     NSString *pplString = (totalPeople == 1 ? @"person" : @"people");
     self.lblPeople.text = [NSString stringWithFormat:@"People: (%d %@)", totalPeople, pplString];
 }
--- TipCalculator/TipCalculator/TipViewController.m
+++ TipCalculator/TipCalculator/TipViewController.m
@@ -35,6 +35,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
+        // Set the title of the View
         self.title = @"Tip Calculator";
     }
     return self;
@@ -42,20 +44,24 @@
 
 - (void) viewWillAppear:(BOOL)animated
 {
+    // Load User Defaults (as set in SettingsViewController)
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *btnTipIndex = [defaults objectForKey:@"btnTipIndex"];
     NSString *btnPeopleValue = [defaults objectForKey:@"btnPeopleValue"];
     
+    // Check if value isSet
     if(btnTipIndex != nil)
     {
         self.btnTip.selectedSegmentIndex = [btnTipIndex floatValue];
     }
     
+    // Check if value isSet
     if(btnPeopleValue != nil)
     {
         self.btnPeople.value = [btnPeopleValue floatValue];
     }
     
+    // Update UI
     [self updateUI];
 }
 
@@ -64,37 +70,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    // Set the buttons left/right
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Keyboard" style:UIBarButtonItemStylePlain target:self action:@selector(onKeyboardClick)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Settings" style:UIBarButtonItemStylePlain target:self action:@selector(onSettingsClick)];
 
+    // Set focus to txtBill
     [self.txtBill becomeFirstResponder];
 }
 
 - (IBAction) onChange:(id)sender
 {
+    // On any change, Update UI
     [self updateUI];
 }
 
 - (void) onKeyboardClick
 {
+    // Close the keyboard
     [self.view endEditing:YES];
 }
 
 - (void) onSettingsClick
 {
+    // Animate to the SettingsViewController
     [self.navigationController pushViewController:[[SettingsViewController alloc] init] animated:YES];
 }
 
 - (void) updateUI
 {
+    // Build an array of values, based on what on Segmented Control
     NSArray *tipValues = @[@(0.15), @(0.18), @(0.20), @(0.22), @(0.25)];
     float tipVal = [tipValues[self.btnTip.selectedSegmentIndex] floatValue];
     
+    // Load UI values into variables and do calculations
     int totalPeople = self.btnPeople.value;
     float billAmount = [self.txtBill.text floatValue] / totalPeople;
     float tipAmount = [self roundValue:(billAmount * tipVal) extraValue:fmodf(billAmount, 1.0f)];
     float totalAmount = [self roundValue:(billAmount + tipAmount)];
     
+    // Update UI with calculated values
     NSString *pplString = (totalPeople == 1 ? @"person" : @"people");
     
     self.lblPeople.text = [NSString stringWithFormat:@"(%d %@)", totalPeople, pplString];
@@ -105,29 +119,36 @@
 
 - (float) roundValue:(float)rawValue
 {
+    // Return ceiling of value (2 decimal places)
     return ceilf(rawValue * 100) / 100;
 }
 
 - (float) roundValue:(float)rawValue extraValue:(float)extraValue
 {
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSString *btnRoundingIndex = [defaults objectForKey:@"btnRoundingIndex"];
+    // Load Rounding information from User Defaults (as set in SettingsViewController)
+    NSString *btnRoundingIndex = [[NSUserDefaults standardUserDefaults] objectForKey:@"btnRoundingIndex"];
     
+    // Check if value isSet
     if(btnRoundingIndex != nil)
     {
         int roundingMethod = [btnRoundingIndex floatValue];
+        
+        // Value must always be less than 1
+        extraValue = fmodf(extraValue + rawValue, 1.0f);
     
-        switch (roundingMethod) {
+        switch (roundingMethod)
+        {
             case 0:
-                rawValue -= fmodf(extraValue + rawValue, 1.0f);
+                rawValue -= extraValue;
                 break;
             
             case 2:
-                rawValue += fmodf(1.0f - fmodf(extraValue + rawValue, 1.0f), 1.0f);
+                rawValue += fmodf(1.0f - extraValue, 1.0f);
                 break;
         }
     }
     
+    // return floor of value (2 decimal places)
     return floorf(rawValue * 100) / 100;
 }
 
